668682b0974ba12c4e155066e254b4de8259dd63
Richard Bennett <rbennett485-gmail-com>
2012-12-09 23:17:19 +0000

    q2 now complete

--- SmartPhoneImpl.java
+++ SmartPhoneImpl.java
@@ -9,4 +9,15 @@ public class SmartPhoneImpl extends MobilePhoneImpl {
 		return position;
 	}
 	
+	@Override
+	public void call(String number) {
+		if(number.substring(0,2).equals("00")) {
+			System.out.println("Calling " + number + 
+				" through the internet to save money");
+			return;
+		} else {
+			super.call(number);
+		}
+	}
+	
 }
